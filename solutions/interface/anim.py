082e2cbc71e9587a139339fe6f7f8ddc44be26de
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 20:20:05 +0300

    design

--- solutions/interface/anim.py
+++ solutions/interface/anim.py
@@ -1,15 +1,18 @@
 import threading
 import time
 
+from solutions.interface.console import ConsoleLogger
+
 
 class Animation(threading.Thread):
 
     _UPDATE_DELAY = 0.1
 
-    __slots__ = ("_base_msg", "_track_var")
+    __slots__ = ("__weakref__", "_base_msg", "_prefix", "_track_var")
 
-    def __init__(self, *, base_msg: str, track_var: int) -> None:
+    def __init__(self, *, base_msg: str, prefix: str, track_var: int) -> None:
         self._base_msg = base_msg
+        self._prefix = prefix
         self._track_var = track_var
         super().__init__()
 
@@ -21,6 +24,10 @@ class Animation(threading.Thread):
         return self._base_msg
 
     @property
+    def prefix(self) -> str:
+        return self._prefix
+
+    @property
     def track_var(self) -> int:
         return self._track_var
 
@@ -28,13 +35,18 @@ class Animation(threading.Thread):
     def msg(self) -> str:
         return self._base_msg.format(self._track_var)
 
-    def _log_to_console(self) -> None:
-        raise NotImplementedError
+    def _log_msg(self) -> None:
+        ConsoleLogger.log(message=self.msg, prefix=self.prefix)
+
+    def _log_update(self) -> None:
+        ConsoleLogger.clear_last_line()
+        self._log_msg()
 
     def _update(self) -> None:
         raise NotImplementedError
 
     def run(self) -> None:
+        self._log_msg()
         while True:
             time.sleep(self._UPDATE_DELAY)
             self._update()
