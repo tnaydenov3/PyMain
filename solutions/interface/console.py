18d4cb89255bf3b251baa18ded4b5f6c1e346129
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 18:51:24 +0300

    design

--- solutions/interface/console.py
+++ solutions/interface/console.py
@@ -1,10 +1,17 @@
 class ConsoleLogger:
 
+    _LOG_TEMP = "[{prefix}] {message}"
+
     @staticmethod
     def _log_to_console(message: str) -> None:
         print(message)
 
+    @classmethod
+    def log(cls, message: str, *, prefix: str) -> None:
+        log_msg = cls._LOG_TEMP.format(prefix=prefix, message=message)
+        cls._log_to_console(message=log_msg)
+
     __slots__ = tuple()
 
-    def __init__(self):
+    def __init__(self) -> None:
         raise NotImplementedError
