a252279892aa17d41fb1c9876d9fe81e68716610
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 22:15:21 +0300

    design

--- solutions/file/cls_handler.py
+++ solutions/file/cls_handler.py
@@ -1,6 +1,8 @@
 from solutions.classes.loggable import Loggable
 from solutions.path.path import Path
 
+_ERR_FILE_MISSING = "File {path} does not exist!"
+
 
 class FileHandler(Loggable):
 
@@ -13,6 +15,18 @@ class FileHandler(Loggable):
     def __init__(self, target_path: Path) -> None:
         self._target_path = target_path
 
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self._target_path})"
+
     @property
     def target_path(self) -> Path:
         return self._target_path
+
+    @property
+    def os_path(self) -> str:
+        return self._target_path.os_path
+
+    @Loggable.logfunction
+    def _log_missing(self) -> None:
+        path = self._target_path.os_path
+        self._log_msg(message=_ERR_FILE_MISSING.format(path=path))
