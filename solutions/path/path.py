7c5ce5c4388cb4289253436b3532d1951d418fbd
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 20:56:36 +0300

    implement

--- solutions/path/path.py
+++ solutions/path/path.py
@@ -1,2 +1,9 @@
 class Path:
-    pass
+
+    __slots__ = ("_path_parts",)
+
+    def __init__(self, path_parts: list[str]) -> None:
+        self._path_parts = path_parts
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} ({self._path_parts})>"
