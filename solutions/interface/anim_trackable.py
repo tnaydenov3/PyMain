90e09e2ccf16638c33b49d8d3a91b79978351554
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 20:35:24 +0300

    implement

--- solutions/interface/anim_trackable.py
+++ solutions/interface/anim_trackable.py
@@ -1,2 +1,12 @@
+from typing import Any
+
+
 class AnimTrackable:
-    pass
+
+    __slots__ = ("_value",)
+
+    def __init__(self, value: Any) -> None:
+        self._value = value
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} ({self._value}: {type(self._value)})>"
