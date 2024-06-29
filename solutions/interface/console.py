f1f786e8ccf1fec5567eba731eefecc26b7030b1
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 19:06:56 +0300

    better logger

--- solutions/interface/console.py
+++ solutions/interface/console.py
@@ -1,15 +1,27 @@
+import sys
+
+
 class ConsoleLogger:
+
+    _UTIL_PREV_LINE = "\033[F"
+    _UTIL_CLEAR_PREV_LINE = "\033[F\033[K"
 
     _LOG_TEMP = "[{prefix}] {message}"
 
     @staticmethod
-    def _log_to_console(message: str) -> None:
-        print(message)
+    def _log_to_console(message: str, *, newline: bool = True) -> None:
+        eol = "\n" if newline else ""
+        sys.stdout.write(f"{message}{eol}")
+        sys.stdout.flush()
 
     @classmethod
     def log(cls, message: str, *, prefix: str) -> None:
         log_msg = cls._LOG_TEMP.format(prefix=prefix, message=message)
         cls._log_to_console(message=log_msg)
+
+    @classmethod
+    def clear_last_line(cls) -> None:
+        cls._log_to_console(message=cls._UTIL_CLEAR_PREV_LINE, newline=False)
 
     __slots__ = tuple()
 
