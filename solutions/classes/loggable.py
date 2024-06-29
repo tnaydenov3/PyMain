a892d05db2beea2f67ed10eaaa44fe86989e843d
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 21:54:46 +0300

    implement

--- solutions/classes/loggable.py
+++ solutions/classes/loggable.py
@@ -1,8 +1,15 @@
+from solutions.interface.console import ConsoleLogger
+
+
 class Loggable:
 
     @staticmethod
     def _log_prefix() -> str:
         raise NotImplementedError
+
+    @classmethod
+    def _log_msg(cls, message: str) -> None:
+        ConsoleLogger.log(message=message, prefix=cls._log_prefix())
 
     __slots__ = tuple()
 
