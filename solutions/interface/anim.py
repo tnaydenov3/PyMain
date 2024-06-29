5a0be68770f19c0cd60e310e0c4773c6d1c8abc7
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 20:09:18 +0300

    implement

--- solutions/interface/anim.py
+++ solutions/interface/anim.py
@@ -1,4 +1,17 @@
-class Animation:
+import threading
 
-    def __init__(self) -> None:
-        pass
+
+class Animation(threading.Thread):
+
+    def __init__(self, *, base_msg: str, track_var: int) -> None:
+        self._base_msg = base_msg
+        self._track_var = track_var
+        super().__init__()
+
+    @property
+    def base_msg(self) -> str:
+        return self._base_msg
+
+    @property
+    def track_var(self) -> int:
+        return self._track_var
