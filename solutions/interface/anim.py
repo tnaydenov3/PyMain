37b000730ad59c00ec6672b16198de5ce64fa8fd
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 20:13:58 +0300

    design

--- solutions/interface/anim.py
+++ solutions/interface/anim.py
@@ -3,10 +3,15 @@ import threading
 
 class Animation(threading.Thread):
 
+    __slots__ = ("_base_msg", "_track_var")
+
     def __init__(self, *, base_msg: str, track_var: int) -> None:
         self._base_msg = base_msg
         self._track_var = track_var
         super().__init__()
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} ({self._base_msg})>"
 
     @property
     def base_msg(self) -> str:
@@ -15,3 +20,13 @@ class Animation(threading.Thread):
     @property
     def track_var(self) -> int:
         return self._track_var
+
+    @property
+    def msg(self) -> str:
+        return self._base_msg.format(self._track_var)
+
+    def run(self) -> None:
+        pass
+
+    def stop(self) -> None:
+        self.join()
