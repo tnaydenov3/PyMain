ff1e296b246c7833e543f2deda293848b6360847
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 21:50:19 +0300

    implement

--- solutions/file/cls_handler.py
+++ solutions/file/cls_handler.py
@@ -1,2 +1,13 @@
+from solutions.path.path import Path
+
+
 class FileHandler:
-    pass
+
+    __slots__ = ("_target_path",)
+
+    def __init__(self, target_path: Path) -> None:
+        self._target_path = target_path
+
+    @property
+    def target_path(self) -> Path:
+        return self._target_path
