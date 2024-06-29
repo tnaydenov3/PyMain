77f46a2ae2f91c89dfd12165c0e7e7ca57e7cc99
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 21:53:52 +0300

    implement

--- solutions/classes/loggable.py
+++ solutions/classes/loggable.py
@@ -1,2 +1,13 @@
 class Loggable:
-    pass
+
+    @staticmethod
+    def _log_prefix() -> str:
+        raise NotImplementedError
+
+    __slots__ = tuple()
+
+    def __init__(self) -> None:
+        raise NotImplementedError
+
+    def __repr__(self) -> str:
+        raise NotImplementedError
