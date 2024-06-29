9bf49fd992d30bb4e96c8b6bce3acdd270bd22f8
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-29 20:37:09 +0300

    design

--- solutions/interface/anim_trackable.py
+++ solutions/interface/anim_trackable.py
@@ -1,9 +1,24 @@
 class AnimTrackable:
 
-    __slots__ = ("_value",)
+    __slots__ = ("_value", "_val_type")
 
     def __init__(self, value: object) -> None:
         self._value = value
+        self._val_type = type(self._value)
 
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} ({self._value}: {type(self._value)})>"
+        return f"<{self.__class__.__name__} ({self._value}: {self._val_type})>"
+
+    @property
+    def value(self) -> object:
+        return self._value
+
+    @property
+    def val_type(self) -> type:
+        return self._val_type
+
+    @value.setter
+    def value(self, value: object) -> None:
+        if not isinstance(value, self._val_type):
+            raise TypeError(f"Value must be of type {self._val_type}")
+        self._value = value
