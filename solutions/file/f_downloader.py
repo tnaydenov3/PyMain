70a0100f256f1aa12bbfc295b7bc0c9de6443226
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-30 20:38:27 +0300

    implement

--- solutions/file/f_downloader.py
+++ solutions/file/f_downloader.py
@@ -1,6 +1,24 @@
 from solutions.file.cls_handler import FileHandler
+from solutions.path.path import Path
+
+_DL_ACTION = "Download"
 
 
 class FileDownloader(FileHandler):
 
-    pass
+    @classmethod
+    def _cl_action(cls) -> str:
+        return _DL_ACTION
+
+    __slots__ = ("_target_url",)
+
+    def __init__(self, *, target_url: str, target_path: Path) -> None:
+        super().__init__(target_path=target_path)
+        self._target_url = target_url
+
+    @property
+    def target_url(self) -> str:
+        return self._target_url
+
+    def _handle_work(self) -> None:
+        pass
