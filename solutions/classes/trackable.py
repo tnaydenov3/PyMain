ec43b9b53ac2e0cf56cce3cfa10e7d02d5bf8e98
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-30 21:34:06 +0300

    design

--- solutions/classes/trackable.py
+++ solutions/classes/trackable.py
@@ -1,17 +1,29 @@
 _ERR_VALTYPE = "Value must be of type {val_type}"
+_ERR_VALVALUE = "Illegal value."
 
 
 class Trackable:
 
     @staticmethod
-    def _assert_type(*, value: object, val_type: type) -> None:
-        if not isinstance(value, val_type):
-            raise TypeError(_ERR_VALTYPE.format(val_type=val_type))
+    def _check_type(*, value: object, val_type: type) -> bool:
+        return isinstance(value, val_type)
+
+    @staticmethod
+    def _check_constraints(*, value: object) -> bool:
+        raise NotImplementedError
+
+    @classmethod
+    def _assert_value(cls, *, value: object) -> None:
+        if not cls._check_type(value=value, val_type=cls._val_type):
+            raise TypeError(_ERR_VALTYPE.format(val_type=cls._val_type))
+
+        if not cls._check_constraints(value=value):
+            raise ValueError(_ERR_VALVALUE)
 
     __slots__ = ("_value", "_val_type")
 
     def __init__(self, *, value: object, val_type: type) -> None:
-        self._assert_type(value=value, val_type=val_type)
+        self._check_type(value=value, val_type=val_type)
         self._value = value
         self._val_type = val_type
 
@@ -31,5 +43,5 @@ class Trackable:
 
     @value.setter
     def value(self, value: object) -> None:
-        self._assert_type(value=value, val_type=self._val_type)
+        self._check_type(value=value, val_type=self._val_type)
         self._value = value
