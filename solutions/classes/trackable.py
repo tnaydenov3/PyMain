940586d0edbf3b7c3065e9b051f48a44c7eff764
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-30 21:28:47 +0300

    redesign

--- solutions/classes/trackable.py
+++ solutions/classes/trackable.py
@@ -1,10 +1,19 @@
+_ERR_VALTYPE = "Value must be of type {val_type}"
+
+
 class Trackable:
+
+    @staticmethod
+    def _assert_type(*, value: object, val_type: type) -> None:
+        if not isinstance(value, val_type):
+            raise TypeError(_ERR_VALTYPE.format(val_type=val_type))
 
     __slots__ = ("_value", "_val_type")
 
-    def __init__(self, value: object) -> None:
+    def __init__(self, *, value: object, val_type: type) -> None:
+        self._assert_type(value=value, val_type=val_type)
         self._value = value
-        self._val_type = type(self._value)
+        self._val_type = val_type
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} ({self._value}: {self._val_type})>"
@@ -22,6 +31,5 @@ class Trackable:
 
     @value.setter
     def value(self, value: object) -> None:
-        if not isinstance(value, self._val_type):
-            raise TypeError(f"Value must be of type {self._val_type}")
+        self._assert_type(value=value, val_type=self._val_type)
         self._value = value
