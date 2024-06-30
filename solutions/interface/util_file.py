6db907646a58cd8a540c715d43fa8ff9abe71964
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-30 20:53:56 +0300

    design

--- solutions/interface/util_file.py
+++ solutions/interface/util_file.py
@@ -1,4 +1,23 @@
+FILE_SIZE_UNITS = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+FILE_SIZE_MULTIPLE = 1024
+
+_SIZE_NUM_ROUNDING_DECIMALS = 2
+
+
 class SizeBytesUtil:
+
+    @staticmethod
+    def format_bytes(size_bytes: int) -> tuple[float, str]:
+        size_num = size_bytes
+
+        for unit in FILE_SIZE_UNITS:
+            if size_num < FILE_SIZE_MULTIPLE:
+                break
+            size_num /= FILE_SIZE_MULTIPLE
+
+        size_num = round(number=size_num, digits=_SIZE_NUM_ROUNDING_DECIMALS)
+
+        return size_num, unit
 
     __slots__ = tuple()
 
