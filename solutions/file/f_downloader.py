a6a92e29854a64011f017b96c7bb747d8cd33781
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-30 20:39:34 +0300

    design

--- solutions/file/f_downloader.py
+++ solutions/file/f_downloader.py
@@ -1,3 +1,4 @@
+from solutions.classes.trackable import Trackable
 from solutions.file.cls_handler import FileHandler
 from solutions.path.path import Path
 
@@ -15,10 +16,15 @@ class FileDownloader(FileHandler):
     def __init__(self, *, target_url: str, target_path: Path) -> None:
         super().__init__(target_path=target_path)
         self._target_url = target_url
+        self._downlaoded_bytes = Trackable(value=0)
 
     @property
     def target_url(self) -> str:
         return self._target_url
+    
+    @property
+    def downloaded_bytes(self) -> int:
+        return self._downlaoded_bytes.value
 
     def _handle_work(self) -> None:
         pass
