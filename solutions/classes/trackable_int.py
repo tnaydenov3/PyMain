e05f5fd2508bab1272dfec870f9a3bd093294fdd
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-30 22:03:19 +0300

    redesign

--- solutions/classes/trackable_int.py
+++ solutions/classes/trackable_int.py
@@ -3,17 +3,30 @@ from solutions.classes.trackable import Trackable
 
 class TrackableInt(Trackable):
 
-    __slots__ = ("_max_value", "_min_value")
+    __slots__ = ("_min_value", "_max_value")
 
     def __init__(
         self,
         *,
         value: int,
         err_value_msg: str,
-        max_value: int,
-        min_value: int = 0,
+        min_value: int = None,
+        max_value: int = None,
     ) -> None:
-        self._max_value = max_value
         self._min_value = min_value
+        self._max_value = max_value
 
-        super().__init__(value=value, val_type=int, err_value_msg=err_value_msg)
+        super().__init__(
+            val_type=int,
+            value=value,
+            err_value_msg=err_value_msg,
+        )
+
+    def _check_constraints(self, *, value: int) -> bool:
+        if self._min_value is not None and value < self._min_value:
+            return False
+
+        if self._max_value is not None and value > self._max_value:
+            return False
+
+        return True
