906955405105da6835c19f910d4287d004801e6a
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-30 20:56:29 +0300

    implement

--- solutions/interface/util.py
+++ solutions/interface/util.py
@@ -1,4 +1,15 @@
+from solutions.interface.util_file import SizeBytesUtil
+
+_FILE_SIZE_PATTERN = "{size_num} {unit}"
+
+
 class ConsoleUtil:
+
+    @staticmethod
+    def size_bytes_to_human_readable(size_bytes: int) -> str:
+        size_num, unit = SizeBytesUtil.format_bytes(size_bytes=size_bytes)
+
+        return _FILE_SIZE_PATTERN.format(size_num=size_num, unit=unit)
 
     __slots__ = tuple()
 
