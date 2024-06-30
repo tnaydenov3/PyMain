e7d7c4c7740eb2a317513ffd3b05c2723d865355
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-06-30 21:36:00 +0300

    design

--- solutions/classes/trackable_int.py
+++ solutions/classes/trackable_int.py
@@ -2,4 +2,12 @@ from solutions.classes.trackable import Trackable
 
 
 class TrackableInt(Trackable):
-    pass
+
+    @staticmethod
+    def _check_constraints(*, value: int) -> bool:
+        return value >= 0
+
+    __slots__ = tuple()
+
+    def __init__(self, *, value: int) -> None:
+        super().__init__(value=value, val_type=int)
