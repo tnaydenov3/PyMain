d919f34bfeb8a84956895a1fb3f2d05985e39a2e
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 13:52:24 +0300

    design

--- solutions/command.py
+++ solutions/command.py
@@ -1,4 +1,12 @@
+from solutions.project import PyMainTree
+from solutions.testing.runner import TestRunner
+
+
 class Commands:
+
+    @staticmethod
+    def run_project_tests() -> None:
+        TestRunner().run_tree(tree=PyMainTree.get_project_tree())
 
     __slots__ = tuple()
 
