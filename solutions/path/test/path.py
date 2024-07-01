703fb329ffd2e86a2d3b2f100213a630c3af8483
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 20:32:18 +0300

    TODO path test

--- solutions/path/test/path.py
+++ solutions/path/test/path.py
@@ -1,2 +1,8 @@
 def _init_test() -> tuple[list[str], list[list[str]]]:
-    pass
+    test_paths_strings = [
+        "solutions/path/path.py",
+        "solutions/path/test",
+        r"C:\_GitHub\PyMain\solutions\interface\debug.py",
+    ]
+
+    # TODO: Too much work for now. Will continue this test later.
