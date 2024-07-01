eaca8af398648546c61eac016a6217727a1b2440
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 22:11:32 +0300

    redesign

--- solutions/command.py
+++ solutions/command.py
@@ -1,6 +1,10 @@
-from typing import Any
+from solutions.interface.colors import TextColors
+from solutions.interface.console import ConsoleLogger
 from solutions.project import PyMainTree
 from solutions.testing.runner import TestRunner
+
+_MSG_INVALID_COMMAND = 'Invalid command "{command}".'
+_T_INVALID_COMMAND = TextColors.temp_red(_MSG_INVALID_COMMAND)
 
 
 class Commands:
@@ -21,8 +25,16 @@ class CommandCall:
 
     _COMMANDS = {Commands.TEST: Commands.run_project_tests}
 
-    def __call__(self, command: str, /) -> None:
-        func = self._COMMANDS[command]
+    @staticmethod
+    def _log_invalid_command(*, command: str) -> None:
+        ConsoleLogger.log(_T_INVALID_COMMAND.format(command=command))
+
+    @classmethod
+    def call(cls, command: str, /) -> None:
+        if not command in cls._COMMANDS:
+            cls._log_invalid_command(command=command)
+
+        func = cls._COMMANDS[command]
         func()
 
     __slots__ = tuple()
