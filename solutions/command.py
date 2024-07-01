5b0290de0c4b469472385b16440cd8abaa2f1f40
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 22:03:34 +0300

    design

--- solutions/command.py
+++ solutions/command.py
@@ -1,3 +1,4 @@
+from typing import Any
 from solutions.project import PyMainTree
 from solutions.testing.runner import TestRunner
 
@@ -7,6 +8,22 @@ class Commands:
     @staticmethod
     def run_project_tests() -> None:
         TestRunner().run_tree(tree=PyMainTree.get_project_tree())
+
+    __slots__ = tuple()
+
+    def __init__(self) -> None:
+        raise NotImplementedError
+
+
+class CommandCall:
+
+    TEST = "test"
+
+    _COMMANDS = {TEST: Commands.run_project_tests}
+
+    def __call__(self, command: str, /) -> None:
+        func = self._COMMANDS[command]
+        func()
 
     __slots__ = tuple()
 
