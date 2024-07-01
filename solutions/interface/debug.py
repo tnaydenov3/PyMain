c2636cde6269c7f67b66f8c665d0456f42dd64a1
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 11:28:34 +0300

    design

--- solutions/interface/debug.py
+++ solutions/interface/debug.py
@@ -3,10 +3,11 @@ from typing import Any
 
 class Debug:
 
+    @classmethod
+    def log(cls, debug_val: Any) -> None:
+        pass
+
     __slots__ = tuple()
 
     def __init__(self) -> None:
         raise NotImplementedError
-
-    def __call__(self, debug_msg: str | Any) -> None:
-        pass
