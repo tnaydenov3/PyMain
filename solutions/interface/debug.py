36f8be28594a5d07c3194d8d6d6d60304ac97283
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 11:29:18 +0300

    design

--- solutions/interface/debug.py
+++ solutions/interface/debug.py
@@ -1,11 +1,19 @@
 from typing import Any
 
+from solutions.interface.console import ConsoleLogger
+
+_DEBUG_PREF = "DEBUG"
+
 
 class Debug:
 
+    @staticmethod
+    def _log_debug_val(debug_val: Any) -> None:
+        ConsoleLogger.log(message=debug_val, prefix=_DEBUG_PREF)
+
     @classmethod
     def log(cls, debug_val: Any) -> None:
-        pass
+        cls._log_debug_val(debug_val=debug_val)
 
     __slots__ = tuple()
 
