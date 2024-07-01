ffa49e702266515633ddf6cfe1fbb9187b4e9abc
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 11:12:50 +0300

    implement

--- solutions/testing/manager.py
+++ solutions/testing/manager.py
@@ -7,7 +7,21 @@ _PYMAIN_ROOT = PyMainRoot()
 
 class TestManager(Singleton):
 
+    @staticmethod
+    def _implemented() -> bool:
+        raise NotImplementedError
+
     __slots__ = tuple()
 
     def __init__(self, *, project_root=_PYMAIN_ROOT) -> None:
+        assert self._implemented()
         TestRunner()._root = project_root
+
+
+class PyMainTestManager(TestManager):
+
+    @staticmethod
+    def _implemented() -> bool:
+        return True
+
+    __slots__ = tuple()
