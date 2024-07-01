490f8242f90f58f39274effba1cf755259698cb8
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:40:14 +0300

    design

--- solutions/testing/testpack.py
+++ solutions/testing/testpack.py
@@ -3,10 +3,13 @@ from solutions.testing.testcase import TestCase
 
 class TestPack:
 
-    __slots__ = "_testcases"
+    __slots__ = ("_testcases", "_passed", "_failed", "_error")
 
     def __init__(self) -> None:
         self._testcases = []
+        self._passed = 0
+        self._failed = 0
+        self._error = 0
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} (total_tests: {len(self._testcases)})>"
