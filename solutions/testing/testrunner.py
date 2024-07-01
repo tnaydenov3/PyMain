5db172daf1d341254d3397c40a8ac769675b6c4d
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 11:01:15 +0300

    design

--- solutions/testing/testrunner.py
+++ solutions/testing/testrunner.py
@@ -8,18 +8,25 @@ from solutions.testing.testpack import TestPack
 
 class TestRunner(Singleton):
 
-    __slots__ = ("_root",)
+    __slots__ = ("_root", "_test_pack")
 
     def __init__(self, root: Root) -> None:
         self._root = root.root
+        self._test_pack = TestPack()
 
-    def _load_tests_from_path(self, file_path: Path) -> TestPack:
+    def _load_tests_from_path(self, *, file_path: Path) -> None:
         module_name = file_path.module_form(root=self._root)
         module: ModuleType = __import__(module_name)
 
-        test_pack = TestPack()
         for obj in module.__dict__.values():
             if isinstance(obj, TestCase):
-                test_pack.add_testcase(testcase=obj)
+                self._test_pack.add_testcase(testcase=obj)
 
-        return test_pack
+    def _run_tests_from_path(self, path: Path) -> None:
+        self._load_tests_from_path(file_path=path)
+        self._test_pack.run()
+
+    def run_local(self, file: str) -> None:
+        self._test_pack = TestPack()
+        file_path = Path.from_string(path_str=file)
+        self._run_tests_from_path(file_path=file_path)
