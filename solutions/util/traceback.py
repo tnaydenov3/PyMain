c308f921a503d60925367165ff870337ea8ba94d
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 14:18:21 +0300

    design

--- solutions/util/traceback.py
+++ solutions/util/traceback.py
@@ -1,16 +1,24 @@
-from types import TracebackType
+from types import FrameType
 
 
 class Tracebacks:
 
     @staticmethod
-    def _get_error_frame(error: Exception) -> TracebackType:
+    def _get_error_frame(error: Exception) -> FrameType:
         traceback = error.__traceback__
 
         while traceback.tb_next:
             traceback = traceback.tb_next
 
-        return traceback
+        return traceback.tb_frame
+
+    @classmethod
+    def get_error_frame(cls, error: Exception) -> FrameType:
+        return cls._get_error_frame(error=error)
+
+    @classmethod
+    def get_error_funcname(cls, error: Exception) -> str:
+        return cls._get_error_frame(error=error).f_code.co_name
 
     __slots__ = tuple()
 
