50eb7ab37877d472b9f1efbe92362217cea9c553
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:36:21 +0300

    FIX result display logic

--- solutions/interface/console_t.py
+++ solutions/interface/console_t.py
@@ -1,6 +1,8 @@
 from solutions.interface.util import ConsoleUtil
 from solutions.testing.testcase import TestCase
 
+_MSG_RESULT_VAL_PASSFAIL = "{result}"
+_MSG_RESULT_VAL_ERROR = "{result} ({error})"
 
 _MSG_TESTCASE = "{result} | {module} | {func_name} | {time}"
 
@@ -9,11 +11,21 @@ class ConsoleTestingLoggerUtil:
 
     @classmethod
     def _make_testcase_log_msg(cls, testcase: TestCase) -> str:
-        result = testcase.result
+        result_val = testcase.result
         module = testcase.func_module
         func_name = testcase.func_name
-
         time_ns = testcase.time_ns
+
+        match result_val:
+            case TestCase.PASS | TestCase.FAIL:
+                result = _MSG_RESULT_VAL_PASSFAIL.format(result=result_val)
+            case TestCase.ERROR:
+                result = _MSG_RESULT_VAL_ERROR.format(
+                    result=result_val, error=testcase.error
+                )
+            case _:
+                raise NotImplementedError
+
         time = ConsoleUtil.time_nanosecs_to_human_readable(time_ns=time_ns)
 
         log_message = _MSG_TESTCASE.format(
