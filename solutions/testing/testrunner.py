c4a75220613637f0ac938f806b46c7d465c4b71a
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 10:57:03 +0300

    design

--- solutions/testing/testrunner.py
+++ solutions/testing/testrunner.py
@@ -1,6 +1,8 @@
+from types import ModuleType
 from solutions.classes.singleton import Singleton
 from solutions.path.path import Path
 from solutions.path.root import Root
+from solutions.testing.testcase import TestCase
 from solutions.testing.testpack import TestPack
 
 
@@ -11,5 +13,13 @@ class TestRunner(Singleton):
     def __init__(self, root: Root) -> None:
         self._root = root.root
 
-    def _load_local_tests(self, file_path: Path) -> TestPack:
-        pass
+    def _load_tests_from_path(self, file_path: Path) -> TestPack:
+        module_name = file_path.module_form(root=self._root)
+        module: ModuleType = __import__(module_name)
+
+        test_pack = TestPack()
+        for obj in module.__dict__.values():
+            if isinstance(obj, TestCase):
+                test_pack.add_testcase(testcase=obj)
+
+        return test_pack
