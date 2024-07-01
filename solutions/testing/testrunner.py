4426af5fbd6306cb476f6d9a7d93c6906de64a0b
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 09:20:32 +0300

    implement

--- solutions/testing/testrunner.py
+++ solutions/testing/testrunner.py
@@ -1,5 +1,13 @@
 class TestRunner:
 
+    @staticmethod
+    def run_local(file: str) -> None:
+        pass
+
+    @staticmethod
+    def run_global(file) -> None:
+        pass
+
     __slots__ = tuple()
 
     def __init__(self) -> None:
