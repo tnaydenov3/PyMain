2dab3e75e944b5c13691cf212bcbc565aeafa71a
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:18:17 +0300

    implement

--- solutions/interface/console_t.py
+++ solutions/interface/console_t.py
@@ -1,4 +1,13 @@
+from unittest import TestCase
+
+from solutions.interface.console import ConsoleLogger
+
+
 class TestsLogger:
+
+    @classmethod
+    def _make_testcase_log_msg(cls, testcase: TestCase) -> str:
+        pass
 
     __slots__ = tuple()
 
