c893a271d4ab931ac25628f9bb90743dde2ddd73
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 12:59:11 +0300

    implement

--- solutions/path/ignore.py
+++ solutions/path/ignore.py
@@ -1,7 +1,16 @@
 from solutions.classes.singleton import Singleton
+from solutions.path.path import Path
+from solutions.path.root import Root
+
+_GITIGNORE_FILE = ".gitignore"
 
 
 class IgnoreManager(Singleton):
+
+    @staticmethod
+    def _find_gitignore(root: Root) -> Path:
+        pass
+
     __slots__ = ("_ignore_list",)
 
     def __init__(self) -> None:
