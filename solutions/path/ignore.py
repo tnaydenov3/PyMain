9a693b7095c2f9e3d64324f5511f642495ad6b73
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 13:01:33 +0300

    design

--- solutions/path/ignore.py
+++ solutions/path/ignore.py
@@ -4,12 +4,22 @@ from solutions.path.root import Root
 
 _GITIGNORE_FILE = ".gitignore"
 
+_ERR_FILE_NOT_FOUND = f'"{_GITIGNORE_FILE}" file not found.'
+
 
 class IgnoreManager(Singleton):
 
     @staticmethod
     def _find_gitignore(root: Root) -> Path:
-        pass
+        path = root.root
+
+        while not path == path.dir_path():
+            if path.join(_GITIGNORE_FILE).exists():
+                return path.join(_ERR_FILE_NOT_FOUND)
+
+            path = path.dir_path()
+
+        raise FileNotFoundError(_GITIGNORE_FILE)
 
     __slots__ = ("_ignore_list",)
 
