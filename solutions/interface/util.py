b5218a9470362451c062694eaf47da6951e6657f
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 19:11:35 +0300

    strict design

--- solutions/interface/util.py
+++ solutions/interface/util.py
@@ -7,13 +7,13 @@ _SIZE_UNIT_PATTERN = "{size_num} {unit}"
 class ConsoleUtil:
 
     @staticmethod
-    def size_bytes_to_human_readable(size_bytes: int) -> str:
+    def size_bytes_to_human_readable(*, size_bytes: int) -> str:
         size_num, unit = SizeBytesUtil.format_bytes(size_bytes=size_bytes)
 
         return _SIZE_UNIT_PATTERN.format(size_num=size_num, unit=unit)
 
     @staticmethod
-    def time_nanosecs_to_human_readable(time_ns: int) -> str:
+    def time_nanosecs_to_human_readable(*, time_ns: int) -> str:
         time_num, unit = TimeNanosecondsUtil.format_nanoseconds(time_ns=time_ns)
 
         return _SIZE_UNIT_PATTERN.format(size_num=time_num, unit=unit)
