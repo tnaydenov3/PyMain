be054fd2a7439b19176b64a2345e348801c2b950
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:28:29 +0300

    file renaming

--- solutions/interface/console_t.py
+++ solutions/interface/console_t.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 _MSG_TESTCASE = "{result} | {module} | {func_name} | {time}"
 
 
-class TestingLogerUtil:
+class ConsoleTestingLoggerUtil:
 
     @classmethod
     def _make_testcase_log_msg(cls, testcase: TestCase) -> str:
