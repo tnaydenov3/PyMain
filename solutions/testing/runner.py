be23178b2ee3131a445d3517de73fbe0ad09c0fe
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 20:14:25 +0300

    clearer and consise code

--- solutions/testing/runner.py
+++ solutions/testing/runner.py
@@ -19,36 +19,28 @@ class TestRunner(Singleton):
         self._root: Root = self._project_root().root
         self._test_pack = PyMainTestPack()
 
-    def _load_test_from_module(self, *, module: ModuleType) -> None:
+    def _load_test_from_module(self, *, module_name: str) -> None:
+        module: ModuleType = __import__(name=module_name, fromlist=[module_name])
+
         for obj in module.__dict__.values():
             if isinstance(obj, PyMainTestCase):
                 self._test_pack.add_testcase(testcase=obj)
 
     def _load_tests_from_path(self, *, file_path: Path) -> None:
         module_name = file_path.module_form(root=self._root)
-        module: ModuleType = __import__(name=module_name, fromlist=[module_name])
-
-        self._load_test_from_module(module=module)
+        self._load_test_from_module(module_name=module_name)
 
     def _load_tests_from_tree(self, *, tree: Tree) -> None:
         for module_name in tree.get_module_forms():
-            module: ModuleType = __import__(name=module_name, fromlist=[module_name])
-
-            self._load_test_from_module(module=module)
-
-    def _run_tests_from_path(self, path: Path) -> None:
-        self._load_tests_from_path(file_path=path)
-        self._test_pack.run()
-
-    def _run_tests_from_tree(self, tree: Tree) -> None:
-        self._load_tests_from_tree(tree=tree)
-        self._test_pack.run()
+            self._load_test_from_module(module_name=module_name)
 
     def run_local(self, file: str) -> None:
         self._test_pack = PyMainTestPack()
         file_path = Path.from_string(path_str=file)
-        self._run_tests_from_path(path=file_path)
+        self._load_tests_from_path(path=file_path)
+        self._test_pack.run()
 
     def run_tree(self, tree: Tree) -> None:
         self._test_pack = PyMainTestPack()
-        self._run_tests_from_tree(tree=tree)
+        self._load_tests_from_tree(tree=tree)
+        self._test_pack.run()
