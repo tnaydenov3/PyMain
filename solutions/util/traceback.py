bd790469743c6af7ff6c1950cf2e4f24951fdf37
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 14:16:26 +0300

    design

--- solutions/util/traceback.py
+++ solutions/util/traceback.py
@@ -1,4 +1,16 @@
+from types import TracebackType
+
+
 class Tracebacks:
+
+    @staticmethod
+    def _get_error_frame(error: Exception) -> TracebackType:
+        traceback = error.__traceback__
+
+        while traceback.tb_next:
+            traceback = traceback.tb_next
+
+        return traceback
 
     __slots__ = tuple()
 
