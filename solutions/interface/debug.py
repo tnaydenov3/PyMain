2c63f53241a767cfd2a8ebe911e877fb11ef5ef3
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 19:13:09 +0300

    s/l design

--- solutions/interface/debug.py
+++ solutions/interface/debug.py
@@ -8,11 +8,11 @@ _DEBUG_PREF = "DEBUG"
 class Debug:
 
     @staticmethod
-    def _log_debug_val(debug_val: Any) -> None:
+    def _log_debug_val(*, debug_val: Any) -> None:
         ConsoleLogger.log(message=debug_val, prefix=_DEBUG_PREF)
 
     @classmethod
-    def log(cls, debug_val: Any) -> None:
+    def log(cls, debug_val: Any, /) -> None:
         cls._log_debug_val(debug_val=debug_val)
 
     __slots__ = tuple()
