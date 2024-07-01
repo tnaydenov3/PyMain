35b495dc26f14d05ed19684b0c06b748c3270553
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:12:28 +0300

    design

--- solutions/testing/testcase.py
+++ solutions/testing/testcase.py
@@ -1,3 +1,4 @@
+import time
 from types import FunctionType
 
 
@@ -7,12 +8,12 @@ class TestCase:
     FAIL = "FAIL"
     ERROR = "ERROR"
 
-    __slots__ = ("_func", "_result", "_time")
+    __slots__ = ("_func", "_result", "_time_ns", "_error")
 
     def __init__(self, func: FunctionType) -> None:
         self._func = func
         self._result = None
-        self._time = None
+        self._time_ns = None
         self._error = None
 
     def __repr__(self) -> str:
@@ -35,9 +36,34 @@ class TestCase:
         return self._result
 
     @property
-    def time(self) -> float:
-        return self._time
+    def time_ns(self) -> float:
+        return self._time_ns
 
     @property
     def error(self) -> Exception | None:
         return self._error
+
+    def run(self) -> None:
+        try:
+            start_time = time.perf_counter_ns()
+            self._func()
+            end_time = time.perf_counter_ns()
+            self._result = self.PASS
+
+        except AssertionError as error:
+            end_time = time.perf_counter_ns()
+
+            error_func = error.__traceback__.tb_frame.f_code.co_name
+            if error_func == self._func.__name__:
+                self._result = self.FAIL
+            else:
+                self._result = self.ERROR
+
+            self._error = error
+
+        except Exception as error:
+            end_time = time.perf_counter_ns()
+            self._result = self.ERROR
+            self._error = error
+
+        self._time_ns = end_time - start_time
