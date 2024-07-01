24522a5364762879368c82b6b05bab0637ad6766
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 10:39:31 +0300

    design

--- solutions/_root.py
+++ solutions/_root.py
@@ -1,6 +1,22 @@
+from solutions.classes.singleton import Singleton
+from solutions.path.path import Path
+
+
 class Root(Singleton):
+    _SUBDIR_NEST = 2
+
+    @classmethod
+    def _find_root(cls) -> Path:
+        module = __import__(cls.__module__)
+        cls_file = module.__file__
+
+        path = Path.from_string(path_str=cls_file)
+        for _ in range(cls._SUBDIR_NEST):
+            path = path.dir_path()
+
+        return path
 
     __slots__ = tuple()
 
     def __init__(self) -> None:
-        raise NotImplementedError
+        self._root = self._find_root()
