840d1c1b4ffd1a74e98857ded29aa229b4c977ef
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 21:29:08 +0300

    redesign kwargs

--- solutions/classes/trackable_int.py
+++ solutions/classes/trackable_int.py
@@ -7,11 +7,12 @@ class TrackableInt(Trackable):
 
     def __init__(
         self,
-        *,
+        /,
         value: int,
-        err_value_msg: str,
+        *,
         min_value: int = None,
         max_value: int = None,
+        err_value_msg: str,
     ) -> None:
         self._min_value = min_value
         self._max_value = max_value
@@ -23,10 +24,10 @@ class TrackableInt(Trackable):
         )
 
     def _check_constraints(self, *, value: int) -> bool:
-        if self._min_value is not None and value < self._min_value:
+        if not self._min_value is None and value < self._min_value:
             return False
 
-        if self._max_value is not None and value > self._max_value:
+        if not self._max_value is None and value > self._max_value:
             return False
 
         return True
