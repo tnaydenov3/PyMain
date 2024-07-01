023292e465233ad929b08e26a163314bae7773fd
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:26:17 +0300

    design

--- solutions/interface/util_time.py
+++ solutions/interface/util_time.py
@@ -6,6 +6,19 @@ _TIME_NUM_ROUNDING_DECIMALS = 3
 
 class TimeNanosecondsUtil:
 
+    @staticmethod
+    def format_nanoseconds(time_ns: int) -> tuple[float, str]:
+        time_num = time_ns
+
+        for unit in TIME_UNITS_NS:
+            if time_num < TIME_MULTIPLE:
+                break
+            time_num /= TIME_MULTIPLE
+
+        time_num = round(number=time_num, ndigits=_TIME_NUM_ROUNDING_DECIMALS)
+
+        return time_num, unit
+
     __slots__ = tuple()
 
     def __init__(self) -> None:
