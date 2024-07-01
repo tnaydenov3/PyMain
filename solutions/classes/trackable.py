da8c2fccfb169b7761688b19f62ae072b94ff850
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 21:26:40 +0300

    redesign template

--- solutions/classes/trackable.py
+++ solutions/classes/trackable.py
@@ -1,4 +1,8 @@
-_ERR_VALTYPE_MSG = "Value must be of type {val_type}."
+from solutions.interface.colors import TextColors
+
+
+_ERR_VALTYPE = "Value must be of type {val_type}."
+_T_ERR_VALTYPE = TextColors.temp_red(_ERR_VALTYPE)
 
 
 class Trackable:
@@ -18,6 +22,13 @@ class Trackable:
     def __str__(self) -> str:
         return str(object=self._value)
 
+    def _assert_value(self, *, value: object) -> None:
+        if not isinstance(value, self._val_type):
+            raise TypeError(_T_ERR_VALTYPE.format(val_type=self._val_type))
+
+        if not self._check_constraints(value=value):
+            raise ValueError(self._err_value_msg)
+
     @property
     def value(self) -> object:
         return self._value
@@ -29,10 +40,3 @@ class Trackable:
 
     def _check_constraints(self, *, value: object) -> bool:
         raise NotImplementedError
-
-    def _assert_value(self, *, value: object) -> None:
-        if not isinstance(value, self._val_type):
-            raise TypeError(_ERR_VALTYPE_MSG.format(val_type=self._val_type))
-
-        if not self._check_constraints(value=value):
-            raise ValueError(self._err_value_msg)
