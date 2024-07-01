b8a86f4bc719c96ef99a7bdb8ec4897765ca74f5
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 13:49:48 +0300

    design

--- solutions/testing/runner.py
+++ solutions/testing/runner.py
@@ -2,6 +2,7 @@ from types import ModuleType
 from solutions.classes.singleton import Singleton
 from solutions.path.path import Path
 from solutions.path.root import Root
+from solutions.path.tree import Tree
 from solutions.testing.testcase import PyMainTestCase
 from solutions.testing.testpack import PyMainTestPack
 
@@ -14,16 +15,29 @@ class TestRunner(Singleton):
         self._root: Root = None
         self._test_pack = PyMainTestPack()
 
-    def _load_tests_from_path(self, *, file_path: Path) -> None:
-        module_name = file_path.module_form(root=self._root)
-        module: ModuleType = __import__(name=module_name, fromlist=[module_name])
-
+    def _load_test_from_module(self, *, module: ModuleType) -> None:
         for obj in module.__dict__.values():
             if isinstance(obj, PyMainTestCase):
                 self._test_pack.add_testcase(testcase=obj)
 
+    def _load_tests_from_path(self, *, file_path: Path) -> None:
+        module_name = file_path.module_form(root=self._root)
+        module: ModuleType = __import__(name=module_name, fromlist=[module_name])
+
+        self._load_test_from_module(module=module)
+
+    def _load_tests_from_tree(self, *, tree: Tree) -> None:
+        for module_name in tree.get_module_forms():
+            module: ModuleType = __import__(name=module_name, fromlist=[module_name])
+
+            self._load_test_from_module(module=module)
+
     def _run_tests_from_path(self, path: Path) -> None:
         self._load_tests_from_path(file_path=path)
+        self._test_pack.run()
+
+    def _run_tests_from_tree(self, tree: Tree) -> None:
+        self._load_tests_from_tree(tree=tree)
         self._test_pack.run()
 
     def run_local(self, file: str) -> None:
@@ -31,5 +45,6 @@ class TestRunner(Singleton):
         file_path = Path.from_string(path_str=file)
         self._run_tests_from_path(path=file_path)
 
-    def run_global(self) -> None:
-        pass
+    def run_tree(self, tree: Tree) -> None:
+        self._test_pack = PyMainTestPack()
+        self._run_tests_from_tree(tree=tree)
