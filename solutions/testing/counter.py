44a232416ef0fc02adfb6ba04618cd7d69c491a2
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:48:32 +0300

    implement

---
+++ solutions/testing/counter.py
@@ -0,0 +1,31 @@
+class TestsCounter:
+
+    __slots__ = ("_passed", "_failed", "_error")
+
+    def __init__(self) -> None:
+        self._passed = 0
+        self._failed = 0
+        self._error = 0
+        self._total = 0
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} (total_ran: {self._total})>"
+
+    @property
+    def passed(self) -> int:
+        return self._passed
+
+    @property
+    def failed(self) -> int:
+        return self._failed
+
+    @property
+    def error(self) -> int:
+        return self._error
+
+    @property
+    def total(self) -> int:
+        return self._total
+
+    def all_passed(self) -> bool:
+        return self._passed == self._total
