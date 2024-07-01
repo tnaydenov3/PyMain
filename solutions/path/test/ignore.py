045f14c391009fa4642028cd1fd81dc948e4f29e
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 20:23:00 +0300

    test design

--- solutions/path/test/ignore.py
+++ solutions/path/test/ignore.py
@@ -1,4 +1,10 @@
-def _init_test() -> None:
+from solutions.path.ignore import IgnoreManager
+from solutions.path.path import Path
+from solutions.project import PyMainTestRunner
+from solutions.testing.testcase import PyMainTestCase
+
+
+def _init_test() -> tuple[list[Path], ...]:
     ignore_paths = [
         "solutions/__pycache__",
         r"C:\_GitHub\PyMain\.vscode\launch.json",
@@ -12,4 +18,22 @@ def _init_test() -> None:
         r"C:\_GitHub\PyMain\solutions\interface\debug.py",
     ]
 
+    ignore_paths = [Path.from_string(path_str=path) for path in ignore_paths]
+    not_ignore_paths = [Path.from_string(path_str=path) for path in not_ignore_paths]
+
     return ignore_paths, not_ignore_paths
+
+
+@PyMainTestCase
+def test_ignore() -> None:
+    ignore_paths, not_ignore_paths = _init_test()
+
+    for path in ignore_paths:
+        assert IgnoreManager().is_ignored(path=path)
+
+    for path in not_ignore_paths:
+        assert not IgnoreManager().is_ignored(path=path)
+
+
+if __name__ == "__main__":
+    PyMainTestRunner().run_local(file=__file__)
