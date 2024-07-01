112496814efa6ef8971be4b7715a3ca56fb6a4a7
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 21:21:00 +0300

    redesign

--- solutions/file/f_downloader.py
+++ solutions/file/f_downloader.py
@@ -2,22 +2,30 @@ from solutions.classes.trackable_int import TrackableInt
 from solutions.file.cls_handler import FileHandler
 from solutions.file.sub.f_downloader import SubDownloader
 from solutions.interface.anim import Animation
+from solutions.interface.colors import TextColors
 from solutions.interface.util import ConsoleUtil
 from solutions.path.path import Path
 
-_MIN_DL_SIZE = 0
-_MAX_DL_SIZE = 10 * 1024 * 1024
+_DEF_MIN_DL_SIZE = 0
+_DEF_MAX_DL_SIZE = 10 * 1024 * 1024
 
-_DL_CHUNK_SIZE = 1024
+_DEF_DL_CHUNK_SIZE = 1024
 
 _DL_ACTION = "Download"
 
-_ERR_DL_SIZE_EXCEEDED = f"Download size limit of {_MAX_DL_SIZE} bytes exceeded!"
+_ERR_DL_SIZE_EXCEEDED = f"Download size limit of {_DEF_MAX_DL_SIZE} bytes exceeded!"
+_T_ERR_DL_SIZE_EXCEEDED = TextColors.temp_red(_ERR_DL_SIZE_EXCEEDED)
 
 _ANIM_BASE_MESSAGE = 'Downloading "{url}" to file...[{{}}]'
+_T_ANIM_BASE_MESSAGE = TextColors.temp_green(_ANIM_BASE_MESSAGE)
 
 
 class FileDownloader(FileHandler):
+
+    _MIN_DL_SIZE = _DEF_MIN_DL_SIZE
+    _MAX_DL_SIZE = _DEF_MAX_DL_SIZE
+
+    _DL_CHUNK_SIZE = _DEF_DL_CHUNK_SIZE
 
     @classmethod
     def _cl_action(cls) -> str:
@@ -30,9 +38,9 @@ class FileDownloader(FileHandler):
         self._source_url = source_url
         self._downlaoded_bytes = TrackableInt(
             value=0,
-            err_value_msg=_ERR_DL_SIZE_EXCEEDED,
-            min_value=_MIN_DL_SIZE,
-            max_value=_MAX_DL_SIZE,
+            err_value_msg=_T_ERR_DL_SIZE_EXCEEDED,
+            min_value=self._MIN_DL_SIZE,
+            max_value=self._MAX_DL_SIZE,
         )
 
     def _main_attr(self) -> str:
@@ -46,7 +54,7 @@ class FileDownloader(FileHandler):
     def downloaded_bytes(self) -> int:
         return self._downlaoded_bytes.value
 
-    def download(self, force_dl: bool = False) -> None:
+    def download(self, *, force_dl: bool = False) -> None:
         self._handle(force=force_dl)
 
     def _download_file(self) -> None:
@@ -54,7 +62,7 @@ class FileDownloader(FileHandler):
             source_url=self._source_url,
             target_path=self._target_path,
             trackable=self._downlaoded_bytes,
-            chunk_size=_DL_CHUNK_SIZE,
+            chunk_size=self._DL_CHUNK_SIZE,
         )
 
     def _handle_work(self) -> None:
@@ -73,4 +81,4 @@ class FileDownloader(FileHandler):
             anim_object.stop()
 
     def _anim_base_smg(self) -> str:
-        return _ANIM_BASE_MESSAGE.format(url=self._source_url)
+        return _T_ANIM_BASE_MESSAGE.format(url=self._source_url)
