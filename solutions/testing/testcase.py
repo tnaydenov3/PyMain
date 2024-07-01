ee8ab54688861e00ccbb4c655de348d6a1859002
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:03:12 +0300

    implement

--- solutions/testing/testcase.py
+++ solutions/testing/testcase.py
@@ -3,11 +3,36 @@ from types import FunctionType
 
 class TestCase:
 
-    __slots__ = ("_func",)
+    PASS = "PASS"
+    FAIL = "FAIL"
+    ERROR = "ERROR"
+
+    __slots__ = ("_func", "_result", "_time")
 
     def __init__(self, func: FunctionType) -> None:
         self._func = func
+        self._result = None
+        self._time = None
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} ({self.func.__name__})>"
 
     @property
     def func(self) -> FunctionType:
         return self._func
+
+    @property
+    def func_name(self) -> str:
+        return self._func.__name__
+
+    @property
+    def func_module(self) -> str:
+        return self._func.__module__
+
+    @property
+    def result(self) -> str:
+        return self._result
+
+    @property
+    def time(self) -> float:
+        return self._time
