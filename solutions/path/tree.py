f59df44101a13cfcd3e719442a87616e78bd6620
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 12:43:44 +0300

    implement

--- solutions/path/tree.py
+++ solutions/path/tree.py
@@ -3,6 +3,11 @@ from solutions.path.path import Path
 
 class Tree:
 
+    @classmethod
+    def from_file_string(cls, path_str: str) -> "Tree":
+        root = Path.from_string(path_str=path_str)
+        return cls(root=root)
+
     __slots__ = ("_root", "_tree")
 
     def __init__(self, root: Path) -> None:
