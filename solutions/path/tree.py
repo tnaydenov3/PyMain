f8770c83d2bb570fc2ddb243f344c29cb86e2acb
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 12:42:59 +0300

    implement

--- solutions/path/tree.py
+++ solutions/path/tree.py
@@ -3,11 +3,16 @@ from solutions.path.path import Path
 
 class Tree:
 
-    __slots__ = ("_root",)
+    __slots__ = ("_root", "_tree")
 
     def __init__(self, root: Path) -> None:
         self._root = root
+        self._tree = None
 
     @property
     def root(self) -> Path:
         return self._root
+
+    @property
+    def tree(self) -> list[Path]:
+        return self._tree
