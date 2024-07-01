61a0bad364976ec31a9a8dcfead00b3c07e96ae5
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 22:14:27 +0300

    redesign

--- solutions/command.py
+++ solutions/command.py
@@ -2,6 +2,8 @@ from solutions.interface.colors import TextColors
 from solutions.interface.console import ConsoleLogger
 from solutions.project import PyMainTestRunner, PyMainTree
 
+_LOG_PREFIX = "COMMAND"
+
 _MSG_INVALID_COMMAND = 'Invalid command "{command}".'
 _T_INVALID_COMMAND = TextColors.temp_red(_MSG_INVALID_COMMAND)
 
@@ -26,7 +28,8 @@ class CommandCall:
 
     @staticmethod
     def _log_invalid_command(*, command: str) -> None:
-        ConsoleLogger.log(_T_INVALID_COMMAND.format(command=command))
+        log_msg = _MSG_INVALID_COMMAND.format(command=command)
+        ConsoleLogger.log(log_msg, prefix=_LOG_PREFIX)
 
     @classmethod
     def call(cls, command: str, /) -> None:
