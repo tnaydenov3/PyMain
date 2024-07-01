be2f973657b7af9d553401d0c2c0134bfb515be2
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:39:24 +0300

    implement

--- solutions/testing/testpack.py
+++ solutions/testing/testpack.py
@@ -1,6 +1,16 @@
+from solutions.testing.testcase import TestCase
+
+
 class TestPack:
 
-    __slots__ = tuple()
+    __slots__ = "_testcases"
 
     def __init__(self) -> None:
-        raise NotImplementedError
+        self._testcases = []
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} (total_tests: {len(self._testcases)})>"
+
+    @property
+    def testcases(self) -> list[TestCase]:
+        return self._testcases
