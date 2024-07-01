206893ac9724afb6e38a5b3d0d68d705a9d250f3
Todor Naydenov <59939223+tnaydenov3@users.noreply.github.com>
2024-07-01 08:31:48 +0300

    design

--- solutions/interface/console_t.py
+++ solutions/interface/console_t.py
@@ -1,3 +1,4 @@
+from solutions.interface.util import ConsoleUtil
 from solutions.testing.testcase import TestCase
 
 
@@ -9,6 +10,20 @@ class ConsoleTestingLoggerUtil:
     @classmethod
     def _make_testcase_log_msg(cls, testcase: TestCase) -> str:
         result = testcase.result
+        module = testcase.func_module
+        func_name = testcase.func_name
+
+        time_ns = testcase.time_ns
+        time = ConsoleUtil.time_nanosecs_to_human_readable(time_ns=time_ns)
+
+        log_message = _MSG_TESTCASE.format(
+            result=result,
+            module=module,
+            func_name=func_name,
+            time=time,
+        )
+
+        return log_message
 
     __slots__ = tuple()
 
